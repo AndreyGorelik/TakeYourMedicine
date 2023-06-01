f3cad03b97b886d4f94a8be4fa4824e28fbebc14
AndreyGorelik <bublic007@inbox.ru>
2023-06-01 14:26:22 +0300

    hide and show sheet gestures fixed

--- src/components/ActionSheet.tsx
+++ src/components/ActionSheet.tsx
@@ -11,9 +11,9 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 
-const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
+const { height: screenHeight, width: screenWidth } = Dimensions.get("window");
 
-const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
+const MAX_TRANSLATE_Y = -screenHeight + 50;
 
 interface MenuItem {
   label: string
@@ -54,27 +54,20 @@ const ActionSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
         context.value = { y: translateY.value };
       })
       .onUpdate((event) => {
+        if (event.translationY < 0) return;
         translateY.value = event.translationY + context.value.y;
         translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
       })
       .onEnd(() => {
-        if (translateY.value > -SCREEN_HEIGHT / 3) {
+        if (translateY.value > -450) {
           scrollTo(0);
-        } else if (translateY.value < -SCREEN_HEIGHT / 1.5) {
-          scrollTo(MAX_TRANSLATE_Y);
+        } else {
+          scrollTo(-500);
         }
       });
 
     const rBottomSheetStyle = useAnimatedStyle(() => {
-      const borderRadius = interpolate(
-        translateY.value,
-        [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
-        [25, 5],
-        Extrapolate.CLAMP
-      );
-
       return {
-        borderRadius,
         transform: [{ translateY: translateY.value }],
       };
     });
@@ -128,11 +121,11 @@ const ActionSheet = React.forwardRef<BottomSheetRefProps, BottomSheetProps>(
 
 const styles = StyleSheet.create({
   bottomSheetContainer: {
-    height: SCREEN_HEIGHT,
-    width: SCREEN_WIDTH,
+    height: screenHeight,
+    width: screenWidth,
     backgroundColor: "white",
     position: "absolute",
-    top: SCREEN_HEIGHT,
+    top: screenHeight,
     borderRadius: 25,
   },
   line: {
