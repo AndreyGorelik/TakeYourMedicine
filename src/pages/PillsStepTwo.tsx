8072967b1ec46b4ff355763a6fe06e3c1feb7ae6
AndreyGorelik <bublic007@inbox.ru>
2023-06-09 12:00:50 +0300

    adding pills screen remade

--- src/pages/PillsStepTwo.tsx
+++ src/pages/PillsStepTwo.tsx
@@ -7,79 +7,60 @@ import { RootStackParamList } from '../navigation/AddPills';
 
 type Props = StackScreenProps<RootStackParamList, 'AddMedsStepTwo'>;
 
-interface Notifications {
-  [x: string]: Date;
-}
-
-interface Reminder {
-  [x: string]: any;
-}
-
 function PillsStepTwo(props: Props) {
   const { navigation } = props;
   const { medsRegularity } = props.route.params;
-
-  const notifications: Reminder[] = [];
-  const openedPicker = [];
-
-  for (let i = 0; i < medsRegularity; i++) {
-    const remind = {
-      reminder: new Date(),
-      id: `${i}`,
-    };
-    notifications.push(remind);
-
-    openedPicker.push(false);
-  }
-
-  const [date, setDate] = useState(notifications);
-  const [open, setOpen] = useState(openedPicker);
-
   const { goBack } = navigation;
 
-  const changeDatePickerVisibility = (id: number) => {
-    const temp = open.slice();
-    temp[id] = !temp[id];
+  const [dates, setDates] = useState(Array.from({ length: medsRegularity }, () => new Date()));
+  const [openIndex, setOpenIndex] = useState<null | number>(null);
 
-    setOpen(temp);
+  const handleOpen = (index: number) => {
+    setOpenIndex(index);
   };
 
-  const changeNotificationsDate = (newDate: Date, id: number) => {
-    const temp = [];
-    date.forEach((item) => {
-      temp.push(JSON.parse(JSON.stringify(item)));
+  const handleClose = () => {
+    setOpenIndex(null);
+  };
+
+  const handleConfirm = (date: Date) => {
+    setDates((prevDates) => {
+      const newDates = [...prevDates];
+      newDates[openIndex!] = date;
+      return newDates;
     });
-    temp[id].reminder = newDate;
-    setDate(temp);
+    handleClose();
   };
 
-  const renderItem = ({ item }: Reminder) => {
+  const renderItem = ({ item, index }: { item: Date; index: number }) => {
+    const isOpen = openIndex === index;
     return (
-      <TouchableOpacity style={styles.reminder} onPress={() => changeDatePickerVisibility(item.id)}>
-        <Text>{Number(item.id) + 1} прием</Text>
-        <Text>{`${item.reminder.getHours()}` + ':' + `${item.reminder.getMinutes()}` + '▼'}</Text>
-        <DatePicker
-          modal
-          open={open[item.id]}
-          mode="time"
-          is24hourSource="device"
-          date={item.reminder}
-          onConfirm={(date) => {
-            changeDatePickerVisibility(item.id);
-            changeNotificationsDate(date, item.id);
-          }}
-          onCancel={() => {
-            changeDatePickerVisibility(item.id);
-          }}
-        />
-      </TouchableOpacity>
+      <View>
+        <TouchableOpacity style={styles.reminder} onPress={() => handleOpen(index)}>
+          <Text>{Number(index + 1)} прием</Text>
+          {isOpen && (
+            <DatePicker
+              open={true}
+              mode="time"
+              date={item}
+              onConfirm={handleConfirm}
+              onCancel={handleClose}
+            />
+          )}
+          <Text>{`${item.getHours()}` + ':' + `${item.getMinutes()}` + '▼'}</Text>
+        </TouchableOpacity>
+      </View>
     );
   };
 
   return (
-    <View style={styles.view}>
+    <View>
+      <FlatList
+        data={dates}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
       <Button title="back" onPress={() => goBack()} />
-      <FlatList data={notifications} renderItem={renderItem} keyExtractor={(item) => item.id} />
     </View>
   );
 }
